a59d7417f9082ba65fce12462c461f18f373d0bb
aster pieters <aster.ieters@gmail.com>
2024-10-16 10:37:59 +0200

    comments

--- database/create_user_db.py
+++ database/create_user_db.py
@@ -3,26 +3,21 @@
 import sys
 import os
 
-# Get the relative path to the tools directory
+# Set path to tools
 tools_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../tools'))
-
-# Add the tools directory to the system path
 sys.path.append(tools_path)
 
-# Now you can import execute_query
 from execute_query import execute_query
 
-
-
+# Create user database
 query = """
     CREATE TABLE users (
         user_id SERIAL PRIMARY KEY,
         username VARCHAR(255) UNIQUE NOT NULL,
         password_hash TEXT NOT NULL,
-        salt TEXT NOT NULL,  -- If using a salt separately
+        salt TEXT NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
     """
 
-result = execute_query(query, False)
-print(result)
+execute_query(query, False)
